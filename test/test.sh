244eed9daea37d20666cf0b3ba028e5ec7ba0edd
akisatoon <akisatoon@gmail.com>
2024-10-05 01:55:07 +0900

    change test

--- test/test.sh
+++ test/test.sh
@@ -18,15 +18,15 @@ function assert() {
 }
 
 # test1
-cd "${test_dirs}/test1"
-assert `$bin < "${jsons}/1/true.json"` "true"
-assert `$bin < "${jsons}/1/false1.json"` "false"
-assert `$bin < "${jsons}/1/false2.json"` "false"
-assert `$bin < "${jsons}/1/false3.json"` "false"
+test1_dir="${test_dirs}/test1"
+assert `$bin $test1_dir < "${jsons}/1/true.json"` "true"
+assert `$bin $test1_dir < "${jsons}/1/false1.json"` "false"
+assert `$bin $test1_dir < "${jsons}/1/false2.json"` "false"
+assert `$bin $test1_dir < "${jsons}/1/false3.json"` "false"
 
 # test2
-cd "${test_dirs}/test2"
-assert `$bin < "${jsons}/2/true.json"` "true"
-assert `$bin < "${jsons}/2/false1.json"` "false"
-assert `$bin < "${jsons}/2/false2.json"` "false"
-assert `$bin < "${jsons}/2/false3.json"` "false"
+test2_dir="${test_dirs}/test2"
+assert `$bin $test2_dir < "${jsons}/2/true.json"` "true"
+assert `$bin $test2_dir < "${jsons}/2/false1.json"` "false"
+assert `$bin $test2_dir < "${jsons}/2/false2.json"` "false"
+assert `$bin $test2_dir < "${jsons}/2/false3.json"` "false"
